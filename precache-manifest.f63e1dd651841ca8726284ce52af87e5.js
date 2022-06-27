9c320c548e789ee4a1dcdb23dfc89f44c005cafa
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2022-06-27 12:49:42 -0300

    Updates

---
+++ precache-manifest.f63e1dd651841ca8726284ce52af87e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046b5c5167ac3a2a68c5130ff58d518a",
+    "url": "/AstroMatch/index.html"
+  },
+  {
+    "revision": "7a8890211ffee701564e",
+    "url": "/AstroMatch/static/js/2.2a11b960.chunk.js"
+  },
+  {
+    "revision": "2c282ce7a8739499f0a8",
+    "url": "/AstroMatch/static/js/main.1074417d.chunk.js"
+  },
+  {
+    "revision": "5e551a21bccaf20e3cfc",
+    "url": "/AstroMatch/static/js/runtime~main.5274e790.js"
+  }
+]);
